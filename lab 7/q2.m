b1248f6ddeb1d9fd88a6cf72d44344283ba766a2
ShaunZac <shaunzacharia5@gmail.com>
2021-03-15 14:05:46 +0530

    Update q2.m

--- lab 7/q2.m
+++ lab 7/q2.m
@@ -16,11 +16,14 @@ figure;
 hold on;
 z_range = [0.001, 0.01, 0.1, 0.5, 1];
 overshoots = [];
+i = 1;
 for z=z_range
     C = tf([1, z], [1, z/20]);
-    step(feedback(k*G*C, 1));
+    sys_c = feedback(k*G*C, 1);
+    step(sys_c);
     info_c = stepinfo(sys_c);
     overshoots(i) = info_c.Overshoot;
+    i = i + 1;
     step(sys_c)
 end
 legend("z = " + string(z_range));
